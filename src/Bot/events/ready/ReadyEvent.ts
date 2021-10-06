c749ed4f278f63acc63f79ae5a08645aa3d9835f
subhiashraf912 <subhi.ashraf912@gmail.com>
2021-10-06 19:05:16 +0300

    shitty update

--- src/Bot/events/ready/ReadyEvent.ts
+++ src/Bot/events/ready/ReadyEvent.ts
@@ -1,11 +1,26 @@
 import BaseEvent from "../../utils/structures/BaseEvent";
 import DiscordClient from "../../client/client";
+import { StageChannel } from "discord.js/typings/index.js";
 
 export default class ReadyEvent extends BaseEvent {
   constructor() {
     super("ready");
   }
   async run(client: DiscordClient) {
+    const senGuild = client.guilds.cache.get("783991881028993045");
+    if (senGuild?.me?.voice.channel) {
+      const channel = senGuild.me.voice.channel;
+      client.distube.playVoiceChannel(
+        channel,
+        "https://www.youtube.com/channel/UCiOPAnYULQ0P97xmPDB5Zrw"
+      );
+    } else {
+      const channel = await senGuild?.channels.fetch("895340582032007219");
+      client.distube.playVoiceChannel(
+        channel as StageChannel,
+        "https://www.youtube.com/channel/UCiOPAnYULQ0P97xmPDB5Zrw"
+      );
+    }
     console.log(
       `[${client.user?.tag}] With the ID: [${client.user?.id}] Has Logged In.`
     );
