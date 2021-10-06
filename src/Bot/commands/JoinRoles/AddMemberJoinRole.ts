1e409d8638a97ac5a8ec4f7efa8ffd1f2a65f234
subhiashraf912 <subhi.ashraf912@gmail.com>
2021-10-06 21:22:35 +0300

    loop for sen 24/7

---
+++ src/Bot/commands/JoinRoles/AddMemberJoinRole.ts
@@ -0,0 +1,69 @@
+import { Message } from "discord.js";
+import BaseCommand from "../../utils/structures/BaseCommand";
+import DiscordClient from "../../client/client";
+import getConfig from "../../utils/constants/getConfig";
+import GetLanguage from "../../utils/Languages";
+import getRole from "../../utils/constants/getRole";
+import { GuildConfig } from "../../utils/MongoDB/Models";
+import configType from "../../utils/types/GuildConfig";
+
+export default class Command extends BaseCommand {
+  constructor() {
+    super({
+      name: "add-member-join-role",
+      category: "join roles",
+      aliases: [],
+      userPermissions: [],
+      botPermissions: [],
+      tutorialGif: "",
+    });
+  }
+
+  async run(client: DiscordClient, message: Message, args: Array<string>) {
+    const cachedConfig = await getConfig(client, message.guild?.id as string);
+    let { memberJoinRoles } = cachedConfig;
+    if (!memberJoinRoles) memberJoinRoles = [];
+    const role = await getRole({ message, query: args.join(" ") });
+    if (!role) {
+      message.reply({
+        content: GetLanguage("RoleNotFound", cachedConfig.language),
+      });
+      return;
+    }
+    if (memberJoinRoles.includes(role.id)) {
+      message.reply({
+        content: GetLanguage(
+          "RoleAlreadyExistsInTheDatabase",
+          cachedConfig.language
+        ),
+      });
+      return;
+    }
+    if (!role.editable) {
+      message.reply({
+        content: GetLanguage(
+          "BotIsMissingEditRolePerms",
+          cachedConfig.language
+        ),
+      });
+      return;
+    }
+    memberJoinRoles.push(role.id);
+    const config = await GuildConfig.findOneAndUpdate(
+      {
+        guildId: message.guild?.id,
+      },
+      {
+        memberJoinRoles,
+      },
+      { new: true }
+    );
+
+    client.configs.set(message.guild?.id as string, config as configType);
+    await message.reply({
+      content: GetLanguage("AddedNewMemberJoinRole", cachedConfig.language)
+        .replace("{role}", role.name)
+        .replace("{id}", role.id),
+    });
+  }
+}
---
+++ src/Bot/commands/JoinRoles/BotJoinRoles.ts
@@ -0,0 +1,74 @@
+import { Message, MessageEmbed, Role, TextChannel, User } from "discord.js";
+import BaseCommand from "../../utils/structures/BaseCommand";
+import DiscordClient from "../../client/client";
+import getConfig from "../../utils/constants/getConfig";
+import GetLanguage from "../../utils/Languages";
+import pagination from "../../utils/constants/pagination";
+import getDevelopers from "../../utils/constants/GetDevelopers";
+
+export default class Command extends BaseCommand {
+  constructor() {
+    super({
+      name: "bot-join-roles",
+      category: "join roles",
+      aliases: [],
+      userPermissions: [],
+      botPermissions: [],
+      tutorialGif: "",
+    });
+  }
+
+  async run(client: DiscordClient, message: Message, args: Array<string>) {
+    const config = await getConfig(client, message.guild?.id as string);
+    let { language } = config;
+    const { botJoinRoles } = config;
+    if (!botJoinRoles || !botJoinRoles[0]) {
+      message.reply(GetLanguage("ThereAreNoBotJoinRoles", language));
+      return;
+    }
+    const roles: Role[] = [];
+    botJoinRoles.forEach((roleId) => {
+      const role = message.guild?.roles.cache.get(roleId);
+      role && roles.push(role);
+    });
+    const developer = await getDevelopers({ client });
+    const embeds = generateRolesEmbed(roles, developer, language);
+    pagination({
+      author: message.author,
+      channel: message.channel as TextChannel,
+      embeds,
+      fastSkip: true,
+      pageTravel: true,
+    });
+  }
+}
+
+function generateRolesEmbed(roles: Role[], developer: User, language: string) {
+  const embeds = [];
+  let k = 10;
+  for (let i = 0; i < roles.length; i += 10) {
+    const r = roles.slice(i, k);
+    let j = i;
+    k += 10;
+    let info = "";
+    r.forEach((role) => {
+      info = `${info}\n> **${++j}-**${role} \`id: ${role.id}\`\n`;
+    });
+
+    const embed = new MessageEmbed()
+      .setDescription(
+        `\`\`\`💢${GetLanguage("BotAutoJoinRole", language)}: ${
+          roles.length
+        }💢\`\`\`\`\`\`⚡${GetLanguage(
+          "BotAutoJoinRolesDescsription",
+          language
+        )}⚡\`\`\`${info}`
+      )
+      .setFooter(
+        `${GetLanguage("Developer", language)}: ${developer.tag}`,
+        developer.displayAvatarURL({ dynamic: true })
+      );
+    embeds.push(embed);
+  }
+  return embeds;
+}
