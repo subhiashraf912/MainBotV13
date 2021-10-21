2dfa602150545350ab4103c14c92608a919ea9b8
subhiashraf912 <subhi.ashraf912@gmail.com>
2021-10-21 13:19:08 +0300

    .

--- src/Bot/commands/Levels/VoiceLeaderboard.ts
+++ src/Bot/commands/Levels/VoiceLeaderboard.ts
@@ -10,9 +10,9 @@ import pagination from "../../utils/constants/pagination";
 export default class Command extends BaseCommand {
   constructor() {
     super({
-      name: "",
-      category: "",
-      aliases: [],
+      name: "voice-leaderboard",
+      category: "levels",
+      aliases: ["v-lb"],
       userPermissions: [],
       botPermissions: [],
       tutorialGif: "",
@@ -23,24 +23,15 @@ export default class Command extends BaseCommand {
     const config = await getConfig(client, message.guild?.id as string);
     if (!message.member || !message.guild) return;
 
-    function compare(a: VoiceRankType, b: VoiceRankType) {
-      // Use toUpperCase() to ignore character casing
-
-      let comparison = 0;
-
-      if (a.voiceTime > b.voiceTime) comparison = -1;
-      if (a.voiceTime < b.voiceTime) comparison = 1;
-
-      return comparison;
-    }
-
     let ranks: VoiceRankType[] = await VoiceRankSchema.find({
       server: message.guild.id,
     });
-    if (!ranks)
-      return message.reply({
+    if (!ranks) {
+      message.reply({
         content: GetLanguage("NoRanksWereFound", config.language),
       });
+      return;
+    }
     let TotalXpForUsers: Array<any> = [];
     for (const [key, value] of Object.entries(ranks)) {
       TotalXpForUsers.push({
@@ -48,8 +39,8 @@ export default class Command extends BaseCommand {
         voiceTime: value.voiceTime,
       });
     }
-    TotalXpForUsers.sort(compare);
-    const embeds = await GenerateLeaderboardsEmbed(
+    TotalXpForUsers.sort(this.compare);
+    const embeds = await this.GenerateLeaderboardsEmbed(
       client,
       TotalXpForUsers,
       message.guild
@@ -61,57 +52,67 @@ export default class Command extends BaseCommand {
       pageTravel: true,
     });
   }
-}
+  compare(a: VoiceRankType, b: VoiceRankType) {
+    // Use toUpperCase() to ignore character casing
 
-async function GenerateLeaderboardsEmbed(
-  client: DiscordClient,
-  leaderboards: Array<any>,
-  guild: Guild
-) {
-  const config = await getConfig(client, guild.id);
-  const embeds = [];
-  let k = 5;
-  let x = 0;
-  for (let i = 0; i < leaderboards.length; i += 5) {
-    const current = leaderboards.slice(i, k);
-    let j = i;
-    k += 5;
-    const icon: string = guild.iconURL({ dynamic: true, size: 4096 }) || "";
+    let comparison = 0;
 
-    const embed = new MessageEmbed()
-      .setTitle(GetLanguage("VoiceLeaderboardList", config.language))
-      .setThumbnail(icon);
-    current.forEach((element) => {
-      let member = client.users.cache.find((user) => user.id === element.ID);
-      if (member) {
-        embed.addField(
-          `**${x + 1}-** ${member.username}`,
-          ` \`${GetLanguage(
-            "VoiceTime",
-            config.language
-          )}:\` \`${millisToMinutesAndSeconds(element.voiceTime)} \``
-        );
-      } else {
-        embed.addField(
-          `**${x + 1}-** ${GetLanguage("MemberNotFound", config.language)}`,
-          ` \`${GetLanguage(
-            "VoiceTime",
-            config.language
-          )}:\` \`${millisToMinutesAndSeconds(element.voiceTime)} \``
-        );
-      }
-      x = x + 1;
-    });
+    if (a.voiceTime > b.voiceTime) comparison = -1;
+    if (a.voiceTime < b.voiceTime) comparison = 1;
 
-    embeds.push(embed);
+    return comparison;
   }
-  return embeds;
-}
 
-const millisToMinutesAndSeconds = (timeInMiliseconds: number) => {
-  let h, m, s;
-  h = Math.floor(timeInMiliseconds / 1000 / 60 / 60);
-  m = Math.floor((timeInMiliseconds / 1000 / 60 / 60 - h) * 60);
-  s = Math.floor(((timeInMiliseconds / 1000 / 60 / 60 - h) * 60 - m) * 60);
-  return `${h} Hours - ${m} Minutes - ${s} Secs`;
-};
+  async GenerateLeaderboardsEmbed(
+    client: DiscordClient,
+    leaderboards: Array<any>,
+    guild: Guild
+  ) {
+    const config = await getConfig(client, guild.id);
+    const embeds = [];
+    let k = 5;
+    let x = 0;
+    for (let i = 0; i < leaderboards.length; i += 5) {
+      const current = leaderboards.slice(i, k);
+      let j = i;
+      k += 5;
+      const icon: string = guild.iconURL({ dynamic: true, size: 4096 }) || "";
+
+      const embed = new MessageEmbed()
+        .setTitle(GetLanguage("VoiceLeaderboardList", config.language))
+        .setThumbnail(icon);
+      current.forEach((element) => {
+        let member = client.users.cache.find((user) => user.id === element.ID);
+        if (member) {
+          embed.addField(
+            `**${x + 1}-** ${member.username}`,
+            ` \`${GetLanguage(
+              "VoiceTime",
+              config.language
+            )}:\` \`${this.millisToMinutesAndSeconds(element.voiceTime)} \``
+          );
+        } else {
+          embed.addField(
+            `**${x + 1}-** ${GetLanguage("MemberNotFound", config.language)}`,
+            ` \`${GetLanguage(
+              "VoiceTime",
+              config.language
+            )}:\` \`${this.millisToMinutesAndSeconds(element.voiceTime)} \``
+          );
+        }
+        x = x + 1;
+      });
+
+      embeds.push(embed);
+    }
+    return embeds;
+  }
+
+  millisToMinutesAndSeconds = (timeInMiliseconds: number) => {
+    let h, m, s;
+    h = Math.floor(timeInMiliseconds / 1000 / 60 / 60);
+    m = Math.floor((timeInMiliseconds / 1000 / 60 / 60 - h) * 60);
+    s = Math.floor(((timeInMiliseconds / 1000 / 60 / 60 - h) * 60 - m) * 60);
+    return `${h} Hours - ${m} Minutes - ${s} Secs`;
+  };
+}
