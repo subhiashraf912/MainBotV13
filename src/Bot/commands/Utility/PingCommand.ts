b2022113f3ef79bddbaa0de32b4a786c535b0870
subhiashraf912 <subhi.ashraf912@gmail.com>
2021-10-23 00:07:35 +0300

    added steal sticker command

--- src/Bot/commands/Utility/PingCommand.ts
+++ src/Bot/commands/Utility/PingCommand.ts
@@ -5,23 +5,23 @@ import getConfig from "../../utils/constants/getConfig";
 import GetLanguage from "../../utils/Languages";
 
 export default class PingCommand extends BaseCommand {
-  constructor() {
-    super({
-      name: "ping",
-      category: "utility",
-    });
-  }
+	constructor() {
+		super({
+			name: "ping",
+			category: "utility",
+		});
+	}
 
-  async run(client: DiscordClient, message: Message, args: Array<string>) {
-    const config = await getConfig(client, message.guild?.id as string);
-
-    const embed = new MessageEmbed().setDescription(
-      `\`\`\`${GetLanguage("Latency", config.language)}: ${
-        ((Date.now() - message.createdTimestamp) / 3).toString().split(".")[0]
-      }\`\`\`\n\`\`\`${GetLanguage("APILatency", config.language)}: ${
-        (client.ws.ping / 3).toString().split(".")[0]
-      }\`\`\``
-    ); //
-    message.reply({ embeds: [embed] });
-  }
+	async run(client: DiscordClient, message: Message, args: Array<string>) {
+		const config = await getConfig(client, message.guild?.id as string);
+		message.author.bannerURL({ dynamic: true, size: 4096 });
+		const embed = new MessageEmbed().setDescription(
+			`\`\`\`${GetLanguage("Latency", config.language)}: ${
+				((Date.now() - message.createdTimestamp) / 3).toString().split(".")[0]
+			}\`\`\`\n\`\`\`${GetLanguage("APILatency", config.language)}: ${
+				(client.ws.ping / 3).toString().split(".")[0]
+			}\`\`\``,
+		); //
+		message.reply({ embeds: [embed] });
+	}
 }
---
+++ src/Bot/commands/Utility/StealSticker.ts
@@ -0,0 +1,41 @@
+import { Message } from "discord.js";
+import BaseCommand from "../../utils/structures/BaseCommand";
+import DiscordClient from "../../client/client";
+import getConfig from "../../utils/constants/getConfig";
+import GetLanguage from "../../utils/Languages";
+
+export default class Command extends BaseCommand {
+	constructor() {
+		super({
+			name: "steal-sticker",
+			category: "utility",
+			aliases: [],
+			userPermissions: ["MANAGE_EMOJIS_AND_STICKERS"],
+			botPermissions: ["MANAGE_EMOJIS_AND_STICKERS"],
+			tutorialGif: "",
+		});
+	}
+
+	async run(client: DiscordClient, message: Message, args: Array<string>) {
+		const config = await getConfig(client, message.guild?.id as string);
+		const sticker = message.stickers.first();
+		if (!sticker) {
+			message.reply({
+				content: GetLanguage(
+					"MemberNeedsToSendStickerWithTheMessage",
+					config.language,
+				),
+			});
+			return;
+		}
+		message.guild?.stickers.create(
+			sticker.url,
+			sticker.name,
+			(sticker.tags as string[])[0],
+			{
+				description: sticker.description,
+				reason: `By ${message.author}`,
+			},
+		);
+	}
+}
