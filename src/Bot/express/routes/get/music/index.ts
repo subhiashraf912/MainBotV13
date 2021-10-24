0a997b4c349781923240ce25658fa57bf483c7aa
subhiashraf912 <subhi.ashraf912@gmail.com>
2021-10-24 16:29:00 +0300

    asfsapmo

--- src/Bot/express/routes/get/music/index.ts
+++ src/Bot/express/routes/get/music/index.ts
@@ -1,10 +1,19 @@
 import DiscordClient from "../../../../client/client";
 import { Express, Request, Response } from "express";
 import BaseGet from "../../../classes/bases/BaseGet";
+import { Song } from "distube";
 const GetGuilds = async (app: Express, client: DiscordClient) => {
-	const route = "/music/:guild";
-	const callBack = (Request: Request, Response: Response) => {
-		Response.send(client.distube.getQueue(Request.params.guild)?.songs);
+	const route = "/musicQueue/:guild";
+	const callBack = async (Request: Request, Response: Response) => {
+		const songs = client.distube.getQueue(Request.params.guild)?.songs;
+		if (!songs) {
+			Response.status(404).send({ error: "No Songs in the queue." });
+			return {};
+		}
+		for (const song of songs as Song[]) {
+			await song.user?.fetch();
+		}
+		Response.send(songs);
 		return {};
 	};
 	return new BaseGet({ app, route, callBack });
