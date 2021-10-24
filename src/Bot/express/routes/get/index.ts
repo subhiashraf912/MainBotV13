0a835b9baa07c0e55f7aa29be23a49ea66b3c49f
subhiashraf912 <subhi.ashraf912@gmail.com>
2021-10-24 15:10:32 +0300

    :v

--- src/Bot/express/routes/get/index.ts
+++ src/Bot/express/routes/get/index.ts
@@ -1,10 +1,17 @@
 import DiscordClient from "../../../client/client";
 import { Express, Request, Response } from "express";
 import BaseGet from "../../classes/bases/BaseGet";
+import { join } from "path";
+import express from "express";
 const GetGuilds = async (app: Express, client: DiscordClient) => {
 	const route = "/";
 	const callBack = (Request: Request, Response: Response) => {
-		Response.sendFile("dashboard/index.html");
+		const distPath = join(process.cwd(), "/dashboard");
+		app.use(express.static(distPath));
+		app.use(route, (req, res) => {
+			res.status(200).sendFile(`${distPath}/index.html`);
+		});
+
 		return {};
 	};
 	return new BaseGet({ app, route, callBack });
