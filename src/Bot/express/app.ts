5a09681c487437f4221613a3c9f44b16c55358a8
subhiashraf912 <subhi.ashraf912@gmail.com>
2021-10-24 12:30:17 +0300

    ,,,

--- src/Bot/express/app.ts
+++ src/Bot/express/app.ts
@@ -5,7 +5,8 @@ import { registerGetRoutes } from "../utils/registry";
 const initExpress = (client: DiscordClient) => {
 	const app = express();
 	registerGetRoutes(client, "../express/routes/get");
-	app.listen(3001, "localhost", () => {
+	const port = process.env.PORT ? parseInt(process.env.PORT) : 3001;
+	app.listen(port, "localhost", () => {
 		console.log("listening on port 3001");
 	});
 	return app;
