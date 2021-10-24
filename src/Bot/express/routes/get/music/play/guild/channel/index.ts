c44267ffe32a779c5dd71ddeb7be881aa7828700
subhiashraf912 <subhi.ashraf912@gmail.com>
2021-10-24 15:51:39 +0300

    safsaf

---
+++ src/Bot/express/routes/get/music/play/guild/channel/index.ts
@@ -0,0 +1,42 @@
+import DiscordClient from "../../../../../../../client/client";
+import { Express, Request, Response } from "express";
+import BaseGet from "../../../../../../classes/bases/BaseGet";
+import getConfig from "../../../../../../../utils/constants/getConfig";
+import { VoiceChannel } from "discord.js/typings/index.js";
+const GetGuilds = async (app: Express, client: DiscordClient) => {
+	const route = "/music/play/:guild/:channel/:song";
+	const callBack = async (Request: Request, Response: Response) => {
+		const { guild, channel, song } = Request.params;
+		if (!guild) {
+			Response.status(400).send({ message: "Guild is required" });
+			return {};
+		}
+		if (!channel) {
+			Response.status(400).send({ message: "Channel is required" });
+			return {};
+		}
+		if (!song) {
+			Response.status(400).send({ message: "Song is required" });
+			return {};
+		}
+		const channelObj = client.channels.cache.get(channel);
+		if (!(channelObj instanceof VoiceChannel)) {
+			Response.status(400).send({
+				message: "Channel should be a voice channel",
+			});
+			return {};
+		}
+		try {
+			await client.distube.playVoiceChannel(channelObj, song);
+			Response.status(200).send(
+				(await getConfig(client, Request.params.id)).prefix,
+			);
+		} catch (err: any) {
+			Response.status(400).send({ error: err.message });
+		}
+		return {};
+	};
+	return new BaseGet({ app, route, callBack });
+};
+
+export default GetGuilds;
