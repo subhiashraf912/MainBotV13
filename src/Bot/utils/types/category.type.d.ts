f55cc523534a408a789c4b43fc287c9664f52560
subhiashraf912 <subhi.ashraf912@gmail.com>
2021-10-28 09:24:05 +0300

    inasofna

--- src/Bot/utils/types/category.type.d.ts
+++ src/Bot/utils/types/category.type.d.ts
@@ -1,18 +1,19 @@
-type CategoryString =
-  | "moderation"
-  | "music"
-  | "test"
-  | "utility"
-  | "owner"
-  | "birthdays"
-  | "anime"
-  | "embeds"
-  | "fun"
-  | "games"
-  | "genshin impact"
-  | "giveaways"
-  | "join roles"
-  | "levels"
-  | "logs"
-  | "role playing";
-export default CategoryString;
+interface Category {
+	moderation: "🛠";
+	music: "🎶";
+	test: "🧪";
+	utility: "🔧";
+	owner: "⚕";
+	birthdays: "🎂";
+	anime: "👧";
+	embeds: "📅";
+	fun: "🎈";
+	games: "🎮";
+	"genshin impact": "🧞";
+	giveaways: "🎉";
+	"join roles": "🆕";
+	levels: "🎚";
+	logs: "🔐";
+	"role playing": "🎭";
+}
+export default Category;
