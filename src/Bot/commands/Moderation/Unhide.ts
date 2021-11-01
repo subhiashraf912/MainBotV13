cf8299c637445798e68b3fc2399bd94137634dac
subhiashraf912 <subhi.ashraf912@gmail.com>
2021-11-01 22:55:45 +0200

    asafsa

--- src/Bot/commands/Moderation/Unhide.ts
+++ src/Bot/commands/Moderation/Unhide.ts
@@ -1,3 +1,5 @@
+/** @format */
+
 import { Message } from "discord.js";
 import BaseCommand from "../../utils/structures/BaseCommand";
 import DiscordClient from "../../client/client";
@@ -6,49 +8,49 @@ import GetLanguage from "../../utils/Languages";
 import getChannel from "../../utils/constants/getChannel";
 
 export default class Command extends BaseCommand {
-  constructor() {
-    super({
-      name: "unhide",
-      category: "moderation",
-      aliases: [],
-      userPermissions: ["MANAGE_CHANNELS", "MANAGE_ROLES"],
-      botPermissions: ["MANAGE_CHANNELS", "MANAGE_ROLES"],
-      tutorialGif: "",
-    });
-  }
+	constructor() {
+		super({
+			name: "unhide",
+			category: "moderation",
+			aliases: [],
+			userPermissions: ["MANAGE_CHANNELS", "MANAGE_ROLES"],
+			botPermissions: ["MANAGE_CHANNELS", "MANAGE_ROLES"],
+			tutorialGif: "",
+		});
+	}
 
-  async run(client: DiscordClient, message: Message, args: Array<string>) {
-    if (!message.guild || !message.member) return;
-    const config = await getConfig(client, message.guild.id);
-    const language = config.language;
-    const channel = getChannel({
-      message,
-      query: args.join(" "),
-      returnMessageChannel: true,
-    });
+	async run(client: DiscordClient, message: Message, args: Array<string>) {
+		if (!message.guild || !message.member) return;
+		const config = await getConfig(client, message.guild.id);
+		const language = config.language;
+		const channel = getChannel({
+			message,
+			query: args.join(" "),
+			returnMessageChannel: true,
+		});
 
-    const role = message.guild.roles.cache.find((r) => r.name === "@everyone");
-    if (!channel || !role) return;
-    if (!channel.manageable) {
-      message.reply({
-        content: GetLanguage("ChannelIsNotManageable", language),
-      });
-      return;
-    }
+		const role = message.guild.roles.cache.find((r) => r.name === "@everyone");
+		if (!channel || !role) return;
+		if (!channel.manageable) {
+			message.reply({
+				content: GetLanguage("ChannelIsNotManageable", language),
+			});
+			return;
+		}
 
-    channel.permissionOverwrites
-      .edit(role, {
-        VIEW_CHANNEL: true,
-      })
-      .then(async () => {
-        await message.react("☑");
-        return;
-      })
-      .catch(async (err) => {
-        await message.reply({
-          content: err.message,
-        });
-        return;
-      });
-  }
+		channel.permissionOverwrites
+			.edit(role, {
+				VIEW_CHANNEL: null,
+			})
+			.then(async () => {
+				await message.react("☑");
+				return;
+			})
+			.catch(async (err) => {
+				await message.reply({
+					content: err.message,
+				});
+				return;
+			});
+	}
 }
