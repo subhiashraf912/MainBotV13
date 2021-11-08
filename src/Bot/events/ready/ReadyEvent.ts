7f432e555635a7d55d7e990be20efac212c5d66b
subhiashraf912 <subhi.ashraf912@gmail.com>
2021-11-08 18:17:02 +0200

    Fixed 24/7 channel id

--- src/Bot/events/ready/ReadyEvent.ts
+++ src/Bot/events/ready/ReadyEvent.ts
@@ -17,7 +17,7 @@ export default class ReadyEvent extends BaseEvent {
         );
         client.distube.setRepeatMode(channel, 2);
       } else {
-        const channel = await senGuild?.channels.fetch("895926753166520330");
+        const channel = await senGuild?.channels.fetch("835843120708714526");
         await client.distube.playVoiceChannel(
           channel as StageChannel,
           "https://www.youtube.com/channel/UCiOPAnYULQ0P97xmPDB5Zrw"
