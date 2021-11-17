848bb874da077ae04e890889363531d4e517fb59
subhiashraf912 <subhi.ashraf912@gmail.com>
2021-11-17 13:22:47 +0200

    oioi oi

--- src/Bot/api/controllers/auth/LoginController.ts
+++ src/Bot/api/controllers/auth/LoginController.ts
@@ -5,9 +5,9 @@ export default (client: DiscordClient) => {
   return function loginController(req: Request, res: Response) {
     if (req.user) return res.send(STATUS_CODES.OK);
     const clientId = process.env.DISCORD_CLIENT_ID;
-    const link = `https://discord.com/api/oauth2/authorize?client_id=${clientId}&redirect_uri=${
-      req.protocol
-    }%3A%2F%2F${req.get(
+    const host = req.get('host')!;
+    const protocol = host.includes('localhost') ? "http" : "https";
+    const link = `https://discord.com/api/oauth2/authorize?client_id=${clientId}&redirect_uri=${protocol}%3A%2F%2F${req.get(
       "host"
     )}%2Fapi%2Fauth%2Fdiscord%2Fredirect&response_type=code&scope=identify%20email%20guilds%20guilds.join`;
     res.redirect(link);
