f5cfe45371c92e9151dd41e652a740d04ee89a6a
subhiashraf912 <subhi.ashraf912@gmail.com>
2021-11-19 13:26:49 +0200

    fasf

--- src/Bot/events/message/AiChatService.ts
+++ src/Bot/events/message/AiChatService.ts
@@ -2,7 +2,7 @@ import BaseEvent from "../../utils/structures/BaseEvent";
 import { Message } from "discord.js";
 import DiscordClient from "../../client/client";
 import getConfig from "../../utils/constants/getConfig";
-import Chatbot from "../../utils/Modules/discord-chatbot";
+import { Bot } from 'smart-botto';
 export default class AiChatEvent extends BaseEvent {
   constructor() {
     super("messageCreate");
@@ -16,29 +16,16 @@ export default class AiChatEvent extends BaseEvent {
 
     const config = await getConfig(client, message.guild.id);
     if (message.channelId !== config.aiChatChannel) return;
-    const chatbot = new Chatbot({
-      name: client.user?.username!,
-      gender: 'Female',
-      user: message.author.id,
-    });
-    const args = message.content;
+    const chatbot = new Bot(client.user?.username!);
+    let args = message.content;
 
-              if (args.includes('Aze') || args.includes('aze')) {
-                  args.toLocaleLowerCase().replace('aze', 'udit');
-    }
-    chatbot
-      .chat(args)
-      .then((data: string) => {
-        if (data.toLowerCase().includes('udit')) {
-          let str = data.replaceAll(
-            'Udit',
-            'Aze Sama',
-          ).replaceAll('udit',"Aze Sama")
-          message.reply(str);
-        } else {
-          message.reply(data);
-        }
-      })
-      .catch((e: Error) => console.log(e.message));
+    if (args.includes('Aze') || args.includes('aze'))
+      args = args.toLocaleLowerCase().replace('aze', 'udit');
+    ;
+    chatbot.chat({
+      message: args,
+      user: message.author.id
+    })
+      .then((res) => message.reply(res.response));
   }
 }
