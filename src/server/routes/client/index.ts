86b8f105fdbec732d06bd38d1913289823f2d255
subhiashraf912 <subhi.ashraf912@gmail.com>
2021-11-26 23:01:31 +0200

    forgot thus route

--- src/server/routes/client/index.ts
+++ src/server/routes/client/index.ts
@@ -1,9 +1,11 @@
 import { Router } from "express";
 import DiscordClient from "../../client/classes/client";
 import clientConfigsController from "../../controllers/client/configs";
+import getClientUser from "../../controllers/client/user";
 
 export default (client: DiscordClient) => {
   const router = Router();
   router.get("/configs/:id", clientConfigsController(client));
+  router.get('/user', getClientUser(client));
   return router;
 };
