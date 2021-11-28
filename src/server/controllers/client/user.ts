9b135f924aea968dd3154a8157cb0f0379920b04
subhiashraf912 <subhi.ashraf912@gmail.com>
2021-11-28 12:41:57 +0200

    ah f

--- src/server/controllers/client/user.ts
+++ src/server/controllers/client/user.ts
@@ -5,8 +5,6 @@ import DiscordClient from "../../client/classes/client";
 export default (client: DiscordClient) => {
   return async function getClientUser(req: Request, res: Response) {
     //@ts-ignore
-    if (!req.user) return res.sendStatus(STATUS_CODES.UNAUTHORIZED);
-    //@ts-ignore
     const data = client.user;
     return res.send(data);
   };
