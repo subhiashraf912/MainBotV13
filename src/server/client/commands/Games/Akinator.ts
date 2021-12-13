3177eeadfc6dba97638b69bb56853d578cc8d247
subhiashraf912 <subhi.ashraf912@gmail.com>
2021-12-13 16:57:09 +0200

    ..

--- src/server/client/commands/Games/Akinator.ts
+++ src/server/client/commands/Games/Akinator.ts
@@ -1,17 +1,10 @@
 import {
-  ColorResolvable,
   Message,
-  MessageEmbed,
-  MessageReaction,
-  User,
 } from "discord.js";
 import BaseCommand from "../../utils/structures/BaseCommand";
 import DiscordClient from "../../classes/client";
-import getConfig from "../../utils/constants/getConfig";
-import GetLanguage from "../../utils/Languages";
-import { akiEmojis, colors, emojis } from "../../utils/constants/Emojis";
-const Started = new Set();
-const { Aki } = require("aki-api");
+const akinator = require("discord.js-akinator");
+
 
 export default class Command extends BaseCommand {
   constructor() {
@@ -25,142 +18,24 @@ export default class Command extends BaseCommand {
     });
   }
 
+
   async run(client: DiscordClient, message: Message, args: Array<string>) {
     if (!message.member || !message.guild) return;
-    const config = await getConfig(client, message.guild.id);
-    if (!Started.has(message.author.id)) Started.add(message.author.id);
-    else {
-      message.reply({
-        content: GetLanguage(
-          "GameHasAlreadyStarted",
-          config.language
-        ).replaceAll("{error-emoji}", emojis.err),
-      });
-      return;
-    }
-    const aki = new Aki(config.language);
-    await aki.start();
-    const msg = await message.reply({
-      embeds: [
-        new MessageEmbed()
-          .setTitle(
-            GetLanguage("AkinatorEmbedTitle", config.language)
-              .replaceAll("{author}", message.author.username)
-              .replaceAll("{question}", (aki.currentStep + 1).toString())
-          )
-          .setColor(colors.warn as ColorResolvable)
-          .setDescription(
-            `**${aki.question}**\n${aki.answers
-              .map((x: number, i: number) => `**${x} | ${akiEmojis[i]}**`)
-              .join("\n\n")}`
-          ),
-      ],
-    });
-    for (let emoji of akiEmojis) await msg.react(emoji).catch(console.error);
-    const firstFilter = (reaction: MessageReaction, user: User) =>
-      akiEmojis.includes(reaction.emoji.name as string) &&
-      user.id === message.author.id;
 
-    const collector = msg.createReactionCollector({
-      filter: firstFilter,
-      time: 60000 * 6,
-    });
 
-    collector.on("collect", async (reaction: MessageReaction, user: User) => {
-      reaction.users.remove(user).catch(console.error);
-      if (reaction.emoji.name == "❌") return collector.stop();
-      await aki.step(akiEmojis.indexOf(reaction.emoji.name as string));
-      if (aki.progress >= 70 || aki.currentStep >= 78) {
-        await aki.win();
-        collector.stop();
-        const newMessage = await message.reply({
-          embeds: [
-            new MessageEmbed()
-              .setTitle(
-                GetLanguage("AkinatorThisIsYourCharacter", config.language)
-              )
-              .setDescription(
-                `**${aki.answers[0].name}**\n${GetLanguage(
-                  "RankingAs",
-                  config.language
-                )} **#${aki.answers[0].ranking}**\n\n[${GetLanguage(
-                  "Yes",
-                  config.language
-                )} (👍) / ${GetLanguage("No", config.language)} (👎)]`
-              )
-              .setImage(aki.answers[0].absolute_picture_path)
-              .setColor(colors.warn as ColorResolvable),
-          ],
-        });
-        await newMessage.react("👍");
-        await newMessage.react("👎");
-        const filter = (reaction: MessageReaction, user: User) =>
-          ["👍", "👎", "❌"].includes(reaction.emoji.name as string) &&
-          message.author.id == user.id;
-        const collector2 = newMessage.createReactionCollector({ filter });
-        collector2.on(
-          "collect",
-          async (reaction: MessageReaction, user: User) => {
-            if (reaction.emoji.name === "👍") {
-              reaction.users.remove(user.id);
-              return message.reply({
-                embeds: [
-                  new MessageEmbed()
-                    .setColor(colors.done as ColorResolvable)
-                    .setTitle(
-                      GetLanguage(
-                        "AkinatorBotIsTheWinner",
-                        config.language
-                      ).replaceAll("{emoji}", emojis.done)
-                    ),
-                ],
-              });
-            } else if (reaction.emoji.name === "👎") {
-              reaction.users.remove(user.id);
-              return message.reply({
-                embeds: [
-                  new MessageEmbed()
-                    .setColor(colors.err as ColorResolvable)
-                    .setTitle(
-                      GetLanguage(
-                        "AkinatorMemberIsTheWinner",
-                        config.language
-                      ).replaceAll("{emoji}", emojis.done)
-                    ),
-                ],
-              });
-            } else {
-              collector2.stop();
-            }
-          }
-        );
-        return;
-      }
-      msg.edit({
-        embeds: [
-          new MessageEmbed()
-            .setTitle(
-              `${message.author.username}, ${GetLanguage(
-                "Question",
-                config.language
-              )} ${aki.currentStep + 1}`
-            )
-            .setColor(colors.warn as ColorResolvable)
-            .setDescription(
-              `**${aki.question}**\n${aki.answers
-                .map((x: number, i: number) => `**${x} | ${akiEmojis[i]}**`)
-                .join("\n\n")}`
-            ),
-        ],
-      });
-    });
-    collector.on("end", () => {
-      Started.delete(message.author.id);
-      setTimeout(() => {
-        msg.delete().catch((err: Error) => {
-          console.log(err);
-        });
-      }, 1000);
+    const language = "en"; //The Language of the Game
+const childMode = false; //Whether to use Akinator's Child Mode
+const gameType = "character"; //The Type of Akinator Game to Play. ("animal", "character" or "object")
+const useButtons = true; //Whether to use Discord's Buttons
+const embedColor = "#1F1E33"; //The Color of the Message Embeds
+
+
+    akinator(message, {
+      language: language, //Defaults to "en"
+      childMode: childMode, //Defaults to "false"
+      gameType: gameType, //Defaults to "character"
+      useButtons: useButtons, //Defaults to "false"
+      embedColor: embedColor //Defaults to "RANDOM"
     });
   }
 }
