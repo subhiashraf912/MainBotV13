4683120d088267025cd98976a5c0ed50889695a2
BuildTools <unconfigured@null.spigotmc.org>
2022-05-23 17:43:01 +0000

    safa

--- src/server/api/index.ts
+++ src/server/api/index.ts
@@ -7,7 +7,7 @@ import routes from "../routes";
 import DiscordClient from "../client/classes/client";
 
 const dev = process.env.NODE_ENV !== "production";
-const port = process.env.PORT || 3000;
+const port = process.env.PORT || 3500;
 const initServer = async (client: DiscordClient) => {
   const server = express();
   server.use(cookieParser());
