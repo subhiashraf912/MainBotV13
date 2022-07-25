77e5f432117229391a3fea84c391b144f4ad12c9
Sobhi Ashraf <59281574+subhiashraf912@users.noreply.github.com>
2022-07-25 13:18:05 +0300

    Update ReadyEvent.ts

--- src/server/client/events/ready/ReadyEvent.ts
+++ src/server/client/events/ready/ReadyEvent.ts
@@ -11,14 +11,14 @@ export default class ReadyEvent extends BaseEvent {
       const senGuild = client.guilds.cache.get("783991881028993045");
       if (senGuild?.me?.voice.channel) {
         const channel = senGuild.me.voice.channel;
-        const textChannel = senGuild.channels.cache.get("969509588221198366") as TextChannel;
+        const textChannel = senGuild.channels.cache.get("1001070526250356766") as TextChannel;
         await client.distube.play(channel, "https://www.youtube.com/channel/UCiOPAnYULQ0P97xmPDB5Zrw", {
           textChannel,
         });
         client.distube.setRepeatMode(channel, 2);
       } else {
-        const textChannel = (await client.channels.fetch("969509588221198366")) as GuildTextBasedChannel;
-        const channel = await client.channels.fetch("969508454228164618");
+        const textChannel = (await client.channels.fetch("1001070526250356766")) as GuildTextBasedChannel;
+        const channel = await client.channels.fetch("1001069927756746812");
         await client.distube.play(channel as VoiceChannel, "https://www.youtube.com/channel/UCiOPAnYULQ0P97xmPDB5Zrw", { textChannel });
         client.distube.setRepeatMode(channel as VoiceChannel, 2);
       }
